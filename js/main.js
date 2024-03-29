f80e1cab2931d485d72bed8d5ef3f82c540b9c45
TiThuthur <thurthur.petit.py@gmail.com>
2024-03-29 16:29:51 +0100

    refonte du code pour faire des boucles

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
-const request = "https://dummyjson.com/products/1";
-const content = document.getElementById("content");
+for (let i = 0; i < 20; i++) {
+  const request = "https://dummyjson.com/products/" + i;
+  const content = document.getElementById("content");
 
-const fetching = async (request) => {
-  const response = await fetch(request);
-  const product = await response.json();
-  console.log(product);
-  const card = document.createElement("div");
-  card.className = "card "+product.id;
-  // for (let i = 0; i <div product.images.length; i++) {
-  //   const downloadingImage = new Image();
-  //   downloadingImage.src = product.images[i];
-  //   downloadingImage.onload = () => {
-  //     image.src = this.src;
-  //   };
-  //   downloadingImage.id = "image" + i;
-  //   const newDiv = document.createElement("div");
-  //   newDiv.className = "image";
-  //   newDiv.appendChild(downloadingImage);
-  //   imagesDiv.appendChild(newDiv);
-  // }
-  card.innerHTML = `
-  <div class="title"><h1 id="title" class="roboto-regular">${product.title}</h1></div>
-  <div class="brand"><h2 id="brand" class="roboto-regular">${product.brand}</h2></div>
-  <div class="category"><p id="category" class="roboto-regular">${product.category}</p></div>
-  <div id="description" class="description roboto-regular">${product.description}</div>
+  const fetching = async (request) => {
+    const response = await fetch(request);
+    const product = await response.json();
+    // console.log(product);
+    const card = document.createElement("div");
+    card.className = "card " + product.id;
+    const title = document.createElement("div");
+    title.innerHTML = `<div class="title"><h1 id="title" class="roboto-regular">${product.title}</h1></div>`;
+    const brand = document.createElement("div");
+    brand.innerHTML = `<div class="brand"><h2 id="brand" class="roboto-regular">${product.brand}</h2></div>`;
+    const category = document.createElement("div");
+    category.innerHTML = `<div class="category"><p id="category" class="roboto-regular">${product.category}</p></div>`;
+    const description = document.createElement("div");
+    description.innerHTML = `<div id="description" class="description roboto-regular">${product.description}</div>`;
+    card.innerHTML = `
+  
   <div id="images" class="images">
   <div class="image"><img src="${product.images[0]}" alt="" srcset=""></div>
   <div class="image"><img src="${product.images[1]}" alt="" srcset=""></div>
@@ -54,7 +48,7 @@ const fetching = async (request) => {
     </table>
   </div>`;
 
-  content.appendChild(card);
-  
-};
-fetching(request);
+    content.appendChild(card);
+  };
+  fetching(request);
+}
